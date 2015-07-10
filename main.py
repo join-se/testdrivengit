6ba557008893884f677ca72fea390d685542ddf0
Simon Lindholm <simon.lindholm@afconsult.com>
2015-07-10 16:01:04 +0200

    Added test for equilateral triangles

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ def test_classification():
   classification(45,90,180) #This is not a triangle
 
 def test_sum_angles():
-  classification(60,60,60)
+  classification(70,60,50)
 
 def test_right_angle():
   classification(90,45,45)
+
+def test_equilateral():
+  classification(60,60,60)
 
 def classification(a,b,c):
   if a+b+c == 180:
     print("This IS a triangle")
 
     if a == 90 or b == 90 or c == 90:
-      print("... by the way, it's also a RIGHT triangle")
+      print("  ... and it's also a RIGHT triangle")
+
+    if a == b == c:
+      print("  ... by the way, it's also a EQUILATERAL triangle")
+
   else:
     print("This is not a triangle")
         
@@ -21,4 +28,5 @@ if __name__ == "__main__":
   test_classification()
   test_sum_angles()
   test_right_angle()
+  test_equilateral()
   print("Ending test")
