7c18742e9b21e32aab34ece39b6075a04e3c35bc
Simon Lindholm <simon.lindholm@afconsult.com>
2015-07-10 15:50:39 +0200

    Testing that all angles in a triangle sums up to 180

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 def test_classification():
   classification(45,90,180) #This is not a triangle
-  
+
+def test_sum_angles():
+  classification(60,60,60)
+
 def classification(a,b,c):
-  print("This is not a triangle")
+  if a+b+c == 180:
+    print("This IS a triangle")
+  else:
+    print("This is not a triangle")
         
 if __name__ == "__main__":
   print("Starting test")
   test_classification()
+  test_sum_angles()
   print("Ending test")
