f778fe8c3ed552df19cc7fa5397b0f62950ae08c
Simon Lindholm <simon.lindholm@afconsult.com>
2015-07-10 15:54:37 +0200

    Added a test for right triangles

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ def test_classification():
 def test_sum_angles():
   classification(60,60,60)
 
+def test_right_angle():
+  classification(90,45,45)
+
 def classification(a,b,c):
   if a+b+c == 180:
     print("This IS a triangle")
+
+    if a == 90 or b == 90 or c == 90:
+      print("... by the way, it's also a RIGHT triangle")
   else:
     print("This is not a triangle")
         
@@ -14,4 +20,5 @@ if __name__ == "__main__":
   print("Starting test")
   test_classification()
   test_sum_angles()
+  test_right_angle()
   print("Ending test")
