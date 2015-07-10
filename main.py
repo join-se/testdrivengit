d1e79f9d0c394790575d0a94080a9c2d06285a46
Anders Hedberg <anders.hedberg@videocent.com>
2015-07-10 08:47:40 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+def test_classification():
+  classification(45,90,180) //This is not a triangle
+  
+def classification():
+  print("This is not a triangle");
+  return false
+        
+if __name__ == "__main__":
+  print("Starting test");
+  test_classification();
+  print("Ending test");
